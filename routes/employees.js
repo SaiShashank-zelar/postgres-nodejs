aad84a4af6c2d32ea4085ca7bb31a98b45f48b30
SaiShashank-zelar <shashank@zelarsoft.com>
2021-08-25 19:32:55 +0530

    update

--- routes/employees.js
+++ routes/employees.js
@@ -1,5 +1,5 @@
 const { Client } = require('pg');
-var connectionString = "postgres://shashank:shashank@localhost:5432/sheshu";
+var connectionString = "postgres://sheshu:shashank@localhost:5432/sheshu";
 
 const client = new Client({
     connectionString: connectionString
