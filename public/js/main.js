a1b1cbbf0750a5b4ad8c3a3218b754e8d3294dd9
SaiShashank-zelar <shashank@zelarsoft.com>
2021-08-25 19:31:41 +0530

    update

--- public/js/main.js
+++ public/js/main.js
@@ -7,3 +7,9 @@ function cancelAdd(){
     
     window.location.href = '/employees';
 }
+function editUser(){
+    window.location.href = '/employees/edit/<%=data[i].id%>'
+}
+function deleteUser(){
+    window.location.href = '../employees/delete/<%=data[i].id%>'
+}
